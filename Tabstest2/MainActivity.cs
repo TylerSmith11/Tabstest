9bdefd91d50c8fff24e9fc3df73046e24b7f98cf
TylerSmith11 <tyler@groundspeedhq.com>
2017-05-10 17:24:22 -0400

    * .gitignore: First commit
    * README.md:
    * Tabstest2.sln:
    * packages.config:
    * MainActivity.cs:
    * PagerAdapter.cs:
    * AboutFragment.cs:
    * Tabstest2.csproj:
    * EventsFragment.cs:
    * MembersFragment.cs:
    * AboutAssets.txt:
    * AssemblyInfo.cs:
    * Style.xml:
    * Main.axml:
    * AboutResources.txt:
    * Strings.xml:
    * Members.axml:
    * Icon.png:
    * Icon.png:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * Icon.png:
    * EventFrag.axml:
    * AboutFrag.axml:
    * Icon.png:
    * Icon.png:
    * chamber-logo.png:

---
+++ Tabstest2/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.OS;
+using Android.App;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+
+
+namespace Tabstest2
+{
+	[Activity(Label = "Tabstest2", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : AppCompatActivity
+	{
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+			SetSupportActionBar(toolbar);
+
+			TabLayout tabLayout = FindViewById<TabLayout>(Resource.Id.tab_layout);
+			tabLayout.AddTab(tabLayout.NewTab().SetText("Events"));
+			tabLayout.AddTab(tabLayout.NewTab().SetText("Members"));
+			tabLayout.AddTab(tabLayout.NewTab().SetText("About"));
+
+
+			ViewPager viewPager = FindViewById<ViewPager>(Resource.Id.pager);
+
+		PagerAdapter adapter = new PagerAdapter(SupportFragmentManager, tabLayout.TabCount);
+
+			viewPager.Adapter = adapter;
+			tabLayout.SetupWithViewPager(viewPager);
+
+		}
+	}
+}
+
---
+++ Tabstest2/PagerAdapter.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Support.V4.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace Tabstest2
+{
+	public class PagerAdapter : FragmentStatePagerAdapter
+	{
+		int mNumberofTabs;
+
+
+	
+		public PagerAdapter(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+		{  	
+		}
+
+		public override int Count
+		{
+		get{return mNumberofTabs;}
+		}
+
+		public PagerAdapter(FragmentManager fm, int NumofTabs) : base(fm)
+		{
+			this.mNumberofTabs = NumofTabs;
+		}
+
+
+
+		public override Fragment GetItem(int position)
+		{
+			switch (position)
+			{
+				case 0:
+					EventsFragment tab1 = new EventsFragment();
+					return tab1;
+
+				case 1:
+					MembersFragment tab2 = new MembersFragment();
+					return tab2;
+
+				case 2:
+					AboutFragment tab3 = new AboutFragment();
+					return tab3;
+
+				default:
+					return null;
+			}
+
+
+
+		}
+	}
+}
